4d3d83c1c9d9d42ff229f548a663b49077d60457
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2025-01-10 21:47:16 +0200

    Initial project setup

---
+++ src/main/java/com/streamlined/tasks/SpringcoretaskApplication.java
@@ -0,0 +1,13 @@
+package com.streamlined.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoretaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoretaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streamlined/tasks/SpringcoretaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamlined.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoretaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
