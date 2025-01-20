606212a2515d1876ebbfb3c94ecb7cceb400546e
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2025-01-20 15:42:42 +0200

    Updated TraineeRepositoryImplTest

--- src/test/java/com/streamlined/springhibernatetask/repository/TraineeRepositoryImplTest.java
+++ src/test/java/com/streamlined/springhibernatetask/repository/TraineeRepositoryImplTest.java
@@ -2,7 +2,9 @@ package com.streamlined.springhibernatetask.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -20,6 +22,10 @@ import com.streamlined.springhibernatetask.Utilities;
 import com.streamlined.springhibernatetask.EntityManagerStorage;
 import com.streamlined.springhibernatetask.SpringHibernateTaskApplication;
 import com.streamlined.springhibernatetask.entity.Trainee;
+import com.streamlined.springhibernatetask.entity.Trainer;
+import com.streamlined.springhibernatetask.entity.Training;
+import com.streamlined.springhibernatetask.entity.TrainingType;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Query;
@@ -35,6 +41,10 @@ class TraineeRepositoryImplTest {
     private EntityManagerStorage entityManagerStorage;
     @Autowired
     private TraineeRepositoryImpl traineeRepository;
+    @Autowired
+    private TrainerRepositoryImpl trainerRepository;
+    @Autowired
+    private TrainingRepositoryImpl trainingRepository;
 
     @Test
     void findAllShouldReturnEmptyList_ifTraineeTableContainsNoData() {
@@ -449,6 +459,48 @@ class TraineeRepositoryImplTest {
             if (transaction != null && transaction.isActive())
                 transaction.setRollbackOnly();
         }
+    }
+
+    @Test
+    void getTrainingListByUserNameDateRangeTrainerNameType_shouldReturnListOfTraining_ifSucceeds() {
+        EntityTransaction transaction = null;
+        try (EntityManager entityManager = entityManagerStorage.getEntityManager()) {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+
+            deleteAll(entityManager);
+            TrainingType trainingType = TrainingType.builder().id(9L).name("Chemistry").build();
+            String traineeName = "John.Smith";
+            Trainee trainee = Trainee.builder().firstName("John").lastName("Smith").userName(traineeName)
+                    .passwordHash("john").isActive(true).dateOfBirth(LocalDate.of(1990, 1, 1)).address("USA").build();
+            trainee = traineeRepository.create(trainee);
+            String trainerName = "Richard.Fitz";
+            Trainer trainer = Trainer.builder().firstName("Richard").lastName("Fitz").userName(trainerName)
+                    .passwordHash("john").isActive(true).specialization(trainingType).build();
+            trainer = trainerRepository.create(trainer);
+            Training training = Training.builder().trainee(trainee).trainer(trainer).name("Chemistry")
+                    .type(trainingType).date(LocalDate.of(2024, 1, 1)).duration(Duration.of(1L, ChronoUnit.DAYS))
+                    .build();
+            training = trainingRepository.create(training);
+
+            Set<String> trainingKeys = Utilities
+                    .stream(traineeRepository.getTrainingListByUserNameDateRangeTrainerNameType(traineeName,
+                            LocalDate.of(2023, 1, 1), LocalDate.of(2025, 1, 1), trainerName, trainingType))
+                    .map(this::getTrainingKey).collect(Collectors.toSet());
+
+            assertEquals(1, trainingKeys.size());
+            assertEquals(getTrainingKey(training), trainingKeys.iterator().next());
+        } catch (Exception e) {
+            fail("Exception executing test: ", e);
+        } finally {
+            if (transaction != null && transaction.isActive())
+                transaction.setRollbackOnly();
+        }
+    }
+
+    private String getTrainingKey(Training a) {
+        return "%d%d%d%s%d%tF%s".formatted(a.getId(), a.getTrainee().getId(), a.getTrainer().getId(), a.getName(),
+                a.getType().getId(), a.getDate(), a.getDuration().toString());
     }
 
 }
